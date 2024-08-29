4cb2fbe40908d21a55fd7fd8ad971263e7a0b5e2
Piz20 <eminiantpisani@gmail.com>
2024-08-29 15:38:21 +0100

    deuxieme push and commit

--- Gestion de la flotte/Controllers/HomeController.cs
+++ Gestion de la flotte/Controllers/HomeController.cs
@@ -7,8 +7,11 @@ namespace Gestion_de_la_flotte.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private String piz;
 
+/// <summary>
+/// Commentaire
+/// </summary>
+/// <param name="logger"></param>
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
